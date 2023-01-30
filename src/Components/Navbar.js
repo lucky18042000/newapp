95b6f3f5ca325965e855b4d333d6214a1e69d7e2
lucky18042000 <rohitsingh182000@gmail.com>
2023-01-30 14:43:44 +0530

    final changes

---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../logo192.png'
+function Navbar() {
+  return (
+    <div className='navbar'>
+    <img src={logo} alt="logo"></img>
+    <button>Get User</button>
+    </div>
+  )
+}
+
+export default Navbar
